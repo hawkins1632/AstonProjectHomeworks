7b57d0f8bd4b98bf2297dfe4d276d56b2335aa43
hawkins1632 <sloupock4@gmail.com>
2026-09-04 11:58:31 +0300

    Feature/user dao read (#12)
    * feat: implement hibernate configuration and hibernate util with javadoc
    
    * docs: add extra comments to UserDao
    
    * feat: implement findById and findAll read methods closes #5

--- src/main/java/org/example/dao/UserDao.java
+++ src/main/java/org/example/dao/UserDao.java
@@ -1,4 +1,38 @@
 package org.example.dao;
+import org.example.model.User;
+import org.example.util.HibernateUtil;
+import org.hibernate.Session;
+import java.util.List;
 
 public class UserDao {
+
+
+    /**
+     * Находит пользователя в БД по его уникальному ID
+     *
+     * @param id уникальный идентификатор пользователя в системе
+     * @return объект найденного пользователя User, либо null если запись отсутствует
+     */
+    public User findById(Long id){
+        // Открываем сессию взаимодействия с БД через утильный класс
+       try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+           // Для чтения транзакции не требуются, используем встроенный метод get
+           return session.get(User.class, id);
+       }
+   }
+
+    /**
+     * Извлекает полный список всех зарегистрированных пользователей из базы данных.
+     * Использует язык запросов HQL
+     *
+     * @return список (List) всех существующих объектов User в базе данных
+     */
+   public List<User> findAll(){
+       // Открываем автоматическую сессию в блоке try-with-resources
+       try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+           // Создаём HQL запрос "from User", извлекающий все записи из таблицы
+           return session.createQuery("from User", User.class).list();
+
+       }
+   }
 }
