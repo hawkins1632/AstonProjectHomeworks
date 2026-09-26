adfd8e701857dbea85ff6622fac7855e918f4ebf
hawkins1632 <sloupock4@gmail.com>
2026-09-26 11:23:57 +0300

    Add Swagger/OpenAPI documentation for user-service

--- user-service/src/main/java/org/example/dto/UserResponseDto.java
+++ user-service/src/main/java/org/example/dto/UserResponseDto.java
@@ -1,5 +1,6 @@
 package org.example.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,21 +11,26 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Данные пользователя")
 public class UserResponseDto {
     /**
      * Идентификатор пользователя.
      */
+    @Schema(description = "Идентификатор пользователя", example = "1")
     private Long id;
     /**
      * Имя пользователя.
      */
+    @Schema(description = "Имя пользователя", example = "Ivan")
     private String name;
     /**
      * Электронная почта пользователя.
      */
+    @Schema(description = "Электронная почта", example = "ivan@test.com")
     private String email;
     /**
      * Возраст пользователя.
      */
+    @Schema(description = "Возраст", example = "30")
     private Integer age;
 }
