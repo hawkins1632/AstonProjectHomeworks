a29b6653935e44e81046c6edd1ca4e194db66578
hawkins1632 <sloupock4@gmail.com>
2026-09-26 16:31:46 +0300

    Add Swagger/OpenAPI to notification-service on port 8081

---
+++ notification-service/src/main/java/org/notification/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package org.notification.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI notificationServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Notification Service API")
+                        .description("REST API для управления уведомлениями")
+                        .version("1.0.0"));
+    }
+}
